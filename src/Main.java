dda541cd9af6787572b56f58966a09af73bf33ed
believerofgood <ashurosa@mail.uc.edu>
2025-09-29 13:54:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Enter your age");
+
+        int Age = in.nextInt();
+
+        if (Age >= 21){
+
+            System.out.println("You get a wrist band");
+
+
+
+        }
+
+
+
+
+    }
+}
